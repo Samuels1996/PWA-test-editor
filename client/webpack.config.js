c23da5d707a73e041cb47f84c475fdc8190b39fb
Brian Samuels <samuels.brian9@gmail.com>
2022-05-18 17:14:25 -0400

    fixing webpack as well as adding into install.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,16 +3,16 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
-    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      install: './src/js/install.js'
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
