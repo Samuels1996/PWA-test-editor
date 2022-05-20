3aa4e08c5b0c7811d915a5d9daf2937807ed8d69
Brian Samuels <samuels.brian9@gmail.com>
2022-05-19 21:42:38 -0400

    Making additional changes to folders and files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,20 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+
+//  Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('Getting from the database');
+
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 
 initdb();
